c0d464aa74b7af5a3ec661df73746d452c78f47d
MdFarhan681 <ishrakemon681@gmail.com>
2026-02-01 22:40:57 +0600

    custom btn design

--- src/components/landing/Slider.tsx
+++ src/components/landing/Slider.tsx
@@ -67,7 +67,7 @@ if (!mounted) return null;
                   {slide.subtitle}
                 </p>
 
-                <button className="bg-blue-600 hover:bg-blue-700 transition text-white px-5 py-2.5 rounded-full text-sm sm:text-base font-medium">
+                <button className="my-btn">
                   {slide.button}
                 </button>
               </div>
